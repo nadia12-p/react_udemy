b3db2c09ad7133d2d8696cd3f2d54f01379a420c
Nadia.Pintos <nadia.pintos@EDCMXENLT1064.local>
2022-08-25 17:17:55 -0500

    let and const

--- React/02-intro-javascript/src/index.js
+++ React/02-intro-javascript/src/index.js
@@ -1,1 +1,18 @@
-console.log('Hola mundo');
+
+//variables y constantes
+
+const nombre = 'Nadia';
+const apellido = 'Pintos';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    let valorDado =6;
+
+    console.log(valorDado);
+}
+
+console.log(valorDado);
