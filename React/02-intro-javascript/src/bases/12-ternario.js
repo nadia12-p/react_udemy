723b5b743c18692cd994554db6ca4d4d76ded380
nadia12-p <70818848+nadia12-p@users.noreply.github.com>
2023-02-06 11:43:15 -0600

    operador ternario

---
+++ React/02-intro-javascript/src/bases/12-ternario.js
@@ -0,0 +1,15 @@
+
+const activo = true;
+
+// let mensaje = ``;
+
+// if (activo) {
+//     mensaje = 'Activo',
+// } else {
+//     mensaje = 'Inactivo';
+// }
+
+// const mensaje = (!activo) ? 'Activo' : null;
+const mensaje = (activo === true) && 'Hello';
+
+console.log(mensaje);
