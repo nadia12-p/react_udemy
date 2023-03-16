f05698172156528839186fa332d72ae90dc2077b
nadia12-p <70818848+nadia12-p@users.noreply.github.com>
2023-03-16 16:43:05 -0600

    imprimir variables en el html

--- React/03-counter-app-vite/src/FirsApp.jsx
+++ React/03-counter-app-vite/src/FirsApp.jsx
@@ -1,8 +1,18 @@
+const newMessage = {
+  message: "Hola",
+  title: "Nadia",
+};
+
+const getMessage = () => {
+  const message = newMessage.message + ' ' + newMessage.title;
+  return message
+}
 
 export const FirstApp = () => {
   return (
     <>
-      <h1>Nadia</h1>
+      <h1>{getMessage()}</h1>
+      {/* <code>{JSON.stringify(newMessage)}</code> */}
       <p>Soy un subtitulo</p>
     </>
   );
